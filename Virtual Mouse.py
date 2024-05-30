5618e90857f56e9e2b5be11a0a4872c4efab5e28
Muhammad Ahmed <96483397+Aplha-pro@users.noreply.github.com>
2024-05-30 15:13:44 +0500

    git add

--- Virtual Mouse.py
+++ Virtual Mouse.py
@@ -2,3 +2,17 @@ import cv2
 import numpy as np
 import HandTrackingModule as htm
 import time
+
+cap = cv2.VideoCapture(0)  # 0 means our default camera
+widthCam, heightCam = 640, 480
+
+cap.set(3, widthCam)
+cap.set(4, heightCam)
+
+while True:
+    success, img = cap.read()
+    if success:
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+    else:
+        break
